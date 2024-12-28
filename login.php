6ba8743b222ea347b2cfc1b754b716f7e3eb3c37
Mariam Ashraf <135599579+mariammhassann@users.noreply.github.com>
2024-12-28 22:36:00 +0200

    Update login.php

--- login.php
+++ login.php
@@ -46,7 +46,7 @@ if(isset($_POST['submit'])){
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>login</title>
 
-   <!-- font awesome cdn link  -->
+   <!-- font awesome  link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
    <!-- custom css file link  -->
